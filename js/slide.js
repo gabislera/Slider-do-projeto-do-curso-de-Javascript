d35bac0c4333ea7360a4b00311738f1d605091ec
Gabriela Carniel <gabiicarniel@gmail.com>
2023-02-13 12:16:25 -0300

    navegacao prev e next finalizada

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js'
+
 export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide)
@@ -124,7 +126,11 @@ export default class Slide {
         this.onStart = this.onStart.bind(this)
         this.onMove = this.onMove.bind(this)
         this.onEnd = this.onEnd.bind(this)
-        this.onResize = this.onResize.bind(this)
+
+        this.activePrevSlide = this.activePrevSlide.bind(this)
+        this.activeNextSlide= this.activeNextSlide.bind(this)
+
+        this.onResize = debounce(this.onResize.bind(this), 200)
     }
 
     init() {
@@ -133,6 +139,20 @@ export default class Slide {
         this.addSlideEvents()
         this.slidesConfig()
         this.addResiveEvent()
+        this.changeSlide(2)
         return this
+    }
+}
+
+export class SlideNav extends Slide {
+    addArrow(prev, next) {
+        this.prevElement = document.querySelector(prev)
+        this.nextElement = document.querySelector(next)
+        this.addArrowEvent()
+    }
+
+    addArrowEvent() {
+        this.prevElement.addEventListener('click', this.activePrevSlide)
+        this.nextElement.addEventListener('click', this.activeNextSlide)
     }
 }
